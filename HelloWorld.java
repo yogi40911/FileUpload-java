b6301ab079c1b8087730bde48535c6b0b65f43bd
yogi40911 <67190347+yogi40911@users.noreply.github.com>
2020-06-20 17:53:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
 	public static void main(String args[]){  
-     	System.out.println("Hello Java");  
+     	System.out.println("hello this is my first github jenkinds..!");  
     }  
 }
