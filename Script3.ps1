1916a2d6026dbaa6ef5d80635057ca9133802ebd
yogi40911 <67190347+yogi40911@users.noreply.github.com>
2020-06-20 16:38:42 +0530

    Add files via upload

---
+++ Script3.ps1
@@ -0,0 +1,6 @@
+﻿$name=Get-Date -format "yyyyMMdd"
+New-Item -Path 'E:\TeamCSG\Test_Folder' -ItemType Directory
+Rename-Item -Path "E:\TeamCSG\Test_Folder" -NewName "$name"
+Copy-Item -Path C:\Users\Lenovo\Downloads\OpcExport.xls -Destination E:\TeamCSG\$name
+Rename-Item -Path E:\TeamCSG\$name\OpcExport.xls -NewName OpcExport.xlsx
+Copy-Item -Path C:\Users\Lenovo\Downloads\OpcExport.xls -Destination E:\TeamCSG\$name
